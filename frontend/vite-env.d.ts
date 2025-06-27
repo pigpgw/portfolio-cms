308bc1fa380158c4186b49ecd04ed27c2375d9bd
pigpgw <ceh20002@naver.com>
2025-06-27 16:14:28 +0900

    remote/#11 : 불필요한 환경설정 파일을 삭제한다

--- frontend/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-// src/types/svg.d.ts 또는 src/vite-env.d.ts
-
-declare module '*.svg?react' {
-  import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export { ReactComponent };
-}
