26455935439aed1068b451c098fd32fac5e6f336
pigpgw <ceh20002@naver.com>
2025-06-27 14:53:00 +0900

    stlye/#11 : 전역 기본 폰트를 추가하고 수정한다

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -4,27 +4,18 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive  shadow-sm shadow-gray-400",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive shadow-sm shadow-gray-400",
   {
     variants: {
       variant: {
-        // 등록, 추가, 링크이동
-        primary: 'bg-[#2563EB] text-white hover:bg-[#2B76F4]',
-
-        // 수정
-        secondary: 'bg-[#FFFFFF] text-[#2563EB] border border-[#2563EB] hover:bg-[#F0F7FF]',
-
-        // 취소, 돌아가기
-        ghost: 'bg-[#F1F2F5] text-[#6B7280] hover:text-[#374151]',
-
-        // 미리보기
-        utility: 'bg-[#F76666] text-[#FFFFFF] hover:bg-[#FCA5A5]',
-
-        // 삭제
-        danger: 'bg-[#FECACA] text-[#B91C1C] hover:bg-[#991B1B]',
-
-        // 깃헙 사이트 이동, 상세보기
-        external: 'bg-[#000000] text-white hover:opacity-80',
+        primary:
+          'bg-primary text-primary-foreground hover:bg-secondary hover:text-secondary-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground border border-secondary hover:bg-primary hover:text-primary-foreground',
+        ghost: 'bg-ghost text-ghost-foreground hover:text-[#374151]',
+        utility: 'bg-utility text-utility-foreground hover:bg-[#FCA5A5]',
+        danger: 'bg-danger text-danger-foreground hover:bg-[#991B1B]',
+        external: 'bg-external text-external-foreground hover:opacity-80',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
