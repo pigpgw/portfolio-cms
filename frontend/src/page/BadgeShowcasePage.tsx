ef85a2d2d7b0b34bf0934129b021526c0a0811a3
pigpgw <ceh20002@naver.com>
2025-06-27 18:02:31 +0900

    style/#15 : 뱃지 컴포넌트 style을 수정한다

--- frontend/src/page/BadgeShowcasePage.tsx
+++ frontend/src/page/BadgeShowcasePage.tsx
@@ -1,3 +1,12 @@
+import { Badge } from '@/components/ui/badge';
+
 export const BadgeShowcasePage = () => {
-  return <div>BadgeShowcasePage</div>;
+  return (
+    <div className="flex gap-1">
+      <Badge variant="default">default</Badge>
+      <Badge variant="secondary">secondary</Badge>
+      <Badge variant="destructive">destructive</Badge>
+      <Badge variant="outline">outline</Badge>
+    </div>
+  );
 };
