ac5ea6d1e0db4a9c5e021c56be479fdf0d830271
pigpgw <ceh20002@naver.com>
2025-06-27 13:48:36 +0900

    style/#11 : button 컴포넌트 shadow 속성을 수정한다

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -4,28 +4,27 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive  shadow-sm shadow-gray-400",
   {
     variants: {
       variant: {
         // 등록, 추가, 링크이동
-        primary: 'bg-[#2563EB] text-white hover:bg-[#2B76F4] shadow-xs',
+        primary: 'bg-[#2563EB] text-white hover:bg-[#2B76F4]',
 
         // 수정
-        secondary:
-          'bg-[#FFFFFF] text-[#2563EB] border border-[#2563EB] hover:bg-[#F0F7FF] shadow-xs',
+        secondary: 'bg-[#FFFFFF] text-[#2563EB] border border-[#2563EB] hover:bg-[#F0F7FF]',
 
         // 취소, 돌아가기
-        ghost: 'bg-transparent text-[#6B7280] hover:text-[#374151]',
+        ghost: 'bg-[#F1F2F5] text-[#6B7280] hover:text-[#374151]',
 
         // 미리보기
-        utility: 'bg-[#F76666] text-[#FFFFFF] hover:bg-[#FCA5A5] shadow-xs',
+        utility: 'bg-[#F76666] text-[#FFFFFF] hover:bg-[#FCA5A5]',
 
         // 삭제
-        danger: 'bg-[#FECACA] text-[#B91C1C] hover:bg-[#991B1B] shadow-xs',
+        danger: 'bg-[#FECACA] text-[#B91C1C] hover:bg-[#991B1B]',
 
         // 깃헙 사이트 이동, 상세보기
-        external: 'bg-[#000000] text-white hover:opacity-80 shadow-xs',
+        external: 'bg-[#000000] text-white hover:opacity-80',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
